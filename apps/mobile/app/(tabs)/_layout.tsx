e05bc96fd25af64fcbd17aef09fb7f069f867401
RuiMNFilipe <netofilipe_6@hotmail.com>
2025-08-09 19:52:09 +0100

    fix: authentication flow is now fixed on mobile

--- apps/mobile/app/(tabs)/_layout.tsx
+++ apps/mobile/app/(tabs)/_layout.tsx
@@ -1,32 +1,27 @@
-import { useAuth } from '@/hooks/use-auth';
 import { FontAwesome } from '@expo/vector-icons';
 import { Tabs } from 'expo-router';
 
 export default function TabLayout() {
-  const { isAuthenticated } = useAuth();
-
   return (
     <Tabs screenOptions={{ headerShown: false }}>
-      <Tabs.Protected guard={isAuthenticated}>
-        <Tabs.Screen
-          name="index"
-          options={{
-            title: 'Home',
-            tabBarIcon: ({ color }) => (
-              <FontAwesome size={28} name="home" color={color} />
-            ),
-          }}
-        />
-        <Tabs.Screen
-          name="settings"
-          options={{
-            title: 'Settings',
-            tabBarIcon: ({ color }) => (
-              <FontAwesome size={28} name="cog" color={color} />
-            ),
-          }}
-        />
-      </Tabs.Protected>
+      <Tabs.Screen
+        name="index"
+        options={{
+          title: 'Home',
+          tabBarIcon: ({ color }) => (
+            <FontAwesome size={28} name="home" color={color} />
+          ),
+        }}
+      />
+      <Tabs.Screen
+        name="settings"
+        options={{
+          title: 'Settings',
+          tabBarIcon: ({ color }) => (
+            <FontAwesome size={28} name="cog" color={color} />
+          ),
+        }}
+      />
     </Tabs>
   );
 }
